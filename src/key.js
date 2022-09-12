c2b365e6674d31e2294cec0d4491ecead6492eb7
9nrp <ratchanonnine9@gmail.com>
2022-09-12 22:01:09 +0700

    chore: change api url

--- src/key.js
+++ src/key.js
@@ -1,6 +1,6 @@
 module.exports = {
   // url: "http://35.198.221.55"
-  url: "http://127.0.0.1:5000", // url for local dev
+  // url: "http://127.0.0.1:5000", // url for local dev
   // url: "http://127.0.0.1:8080", // url for docker mern + nginx
-  // url: "https://nu-helmet-sys.herokuapp.com", // url for heroku web server
+  url: "https://nu-helmet-sys.herokuapp.com", // url for heroku web server
 };
